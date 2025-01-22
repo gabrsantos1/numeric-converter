a5d2f058b12125faa9c35797d2e942902fb61d37
Gabriel Santos <gs2625020@gmail.com>
2025-01-22 13:51:17 -0300

    tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        main:{
+          1: "#e4e6d9",
+          2: "#c2cfb4",
+          3: "#8aa98a",
+          4: "#48664e",
+          5: "#25352a",
+        },
+      },
+    },
   },
   plugins: [],
 }
